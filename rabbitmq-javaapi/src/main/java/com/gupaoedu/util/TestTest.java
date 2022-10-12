05f073690a2d38e55921cf756d035d3453d5b12c
suncong <1575711583@qq.com>
2022-10-12 14:37:18 +0800

    gulugulu

---
+++ rabbitmq-javaapi/src/main/java/com/gupaoedu/util/TestTest.java
@@ -0,0 +1,20 @@
+package com.gupaoedu.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Author: qingshan
+ */
+public class TestTest {
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<Integer>();
+
+        for(int i=0; i<100; i++){
+            set1.add(i);
+            set1.remove(i-1);
+        }
+        System.out.println(set1.size());
+    }
+
+}
